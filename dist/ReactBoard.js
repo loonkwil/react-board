4aec712ecf31f8ce12cb61f9194245f35c4682d6
Peter Simon <simonpepe@gmail.com>
2016-02-22 21:17:40 +0100

    Release v1.0.1

--- dist/ReactBoard.js
+++ dist/ReactBoard.js
@@ -1,10 +1,10 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("React"));
+		module.exports = factory(require("react"));
 	else if(typeof define === 'function' && define.amd)
-		define(["React"], factory);
+		define(["react"], factory);
 	else if(typeof exports === 'object')
-		exports["ReactBoard"] = factory(require("React"));
+		exports["ReactBoard"] = factory(require("react"));
 	else
 		root["ReactBoard"] = factory(root["React"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
